d4095005ba599ee1403893dfb2ca7eb90768f35b
Диляра Садыкова <dilyara.mozhet.sadykova@gmail.com>
2021-11-21 13:22:14 +0300

    счетчики цикла перенесены из поля действия

--- ConsoleApp4_1/Program.cs
+++ ConsoleApp4_1/Program.cs
@@ -11,16 +11,12 @@ namespace ConsoleApp4_1
         static void Main(string[] args)
         {
             Console.WriteLine("Введите целое число N>0");
-            Int32 number = Convert.ToInt32(Console.ReadLine()), i = -1;
-
+            Int32 number = Convert.ToInt32(Console.ReadLine());
             if (number > 0)
             {
                 Int32 numSecPow = (Int32)Math.Pow(number, 2);
-                for (Int32 y = 1, x = 0; (y * 2 - 1) + x < numSecPow;)
+                for (Int32 y = 1, x = 0, i = -1; (y * 2 - 1) + x <= numSecPow; i += 2, x += i, y++)
                 {
-                    i = i + 2;
-                    x = x + i;
-                    y++;
                     Int32 u = (y * 2 - 1) + x;
                     Console.WriteLine("{0}", u);
                 }
@@ -30,7 +26,6 @@ namespace ConsoleApp4_1
             {
                 Console.WriteLine("Данные не соответствуют условию");
             }
-
         }
     }
 }
